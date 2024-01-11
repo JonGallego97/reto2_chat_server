c7529004e586f9e01ded970e2551fd0606678b7b
JonGallego97 <jon.gallegoaz@elorrieta-errekamari.com>
2024-01-11 16:02:17 +0100

    first commit

---
+++ src/main/java/com/example/reto2_chat_server/Reto2ChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.reto2_chat_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2ChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2ChatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reto2_chat_server/Reto2ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reto2_chat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto2ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
